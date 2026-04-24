d281a2b6741414f71aa0d7e95ca8cf4197b93527
Yocchi3191 <yoshioyocchi314@gmail.com>
2026-04-24 14:29:15 +0000

    DBスキーマ・マイグレーションを追加

---
+++ drizzle/0000_safe_madripoor.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "nippos" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"content" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
